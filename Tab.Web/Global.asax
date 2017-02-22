191f8655577ed65ec05494500ef7d36171d06d1b
Yanzhenqing <zhenqingyan@126.com>
2017-02-22 23:22:40 +0800

    123

---
+++ Tab.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Tab.Web.MvcApplication" Language="C#" %>
---
+++ Tab.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Tab.Web.Startup))]
+namespace Tab.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
