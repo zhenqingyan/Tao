29c9fcc8403b35346c56d07894904af1980bb547
Yanzhenqing <zhenqingyan@126.com>
2017-03-01 21:39:58 +0800

    搭建框架

--- Apptest/UnitTest1.cs
+++ Apptest/UnitTest1.cs
@@ -9,6 +9,21 @@ namespace Apptest
         [TestMethod]
         public void TestMethod1()
         {
+
+        }
+        public T EnCoding<T>(T entity)
+        {
+            var type = typeof(T);
+            foreach (var item in type.GetProperties())
+            {
+                //通过反射出来值，然后加密后，再重新赋值。
+               item.SetValue(entity, Encoding(item.GetValue(entity).ToString()));
+            }
+            return entity;
+        }
+        public string Encoding(string str)
+        {
+            return null;
         }
     }
 }
---
+++ Tao.Repository/BaseRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Tao.Repository
+{
+    public class BaseRepository
+    {
+
+    }
+}
